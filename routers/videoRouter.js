1e9c04ff1f0c353934627bbdf3f249ad0d597e02
Tenzin Sean <tenzin.jeunesse@gmail.com>
2020-01-23 00:43:19 +0100

    added files

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
+
+import { upload, editVideo } from '../controller/videoController';
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.editProfile, editVideo);
+
 
 export default videoRouter;
