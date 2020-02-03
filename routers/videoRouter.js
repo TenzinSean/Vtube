04b04156d5fc777cfb5690c4475e103183d1e565
Tenzin Sean <tenzin.jeunesse@gmail.com>
2020-02-04 00:41:49 +0100

    added the features

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
 
-import { upload, editVideo, videoDetail } from '../controller/videoController';
+import { upload, editVideo, videoDetail, getUpload, postUpload } from '../controller/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editProfile, editVideo);
 
